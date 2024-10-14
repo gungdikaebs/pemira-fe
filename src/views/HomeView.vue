f5c1e1b0b197454a7bdc45005f38a99b513e1570
gungdikaebs <gungdika88@gmail.com>
2024-10-14 22:30:53 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <section id="hero">
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-12">
+            <div class="text-center">
+              <img src="../components/icons/LogoPemira.png" alt="" class="img-fluid" width="161px" height="159px" />
+              <h1 class="text-shadow main-color font-weight-bold">
+                PEMIRA <br />
+                ITB STIKOM BALI
+              </h1>
+
+              <div class="alert alert-warning col-12 col-md-6 offset-0 offset-md-3" role="alert">
+                Untuk melakukan voting, silahkan login terlebih dahulu dengan akun Microsoft anda.
+                <br>
+                <a href="" class="btn btn-main">LOGIN AKUN MICROSOFT</a>
+                <br>
+                <p style="font-size: 12px">gunakan akun microsoft yang terdaftar di STIKOM BALI
+                  <br>
+                  (nim@stikom-bali.ac.id)
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
---
+++ src/views/VoteView.vue
@@ -0,0 +1,70 @@
+<template>
+  <nav class="bg-light py-3" id="vote">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <div class="text-center">
+            <a class="navbar-brand mx-3">
+              <img src="" alt="Bootstrap" width="60" height="60" />
+              <img src="" alt="Bootstrap" width="60" height="60" />
+              <img src="" alt="Bootstrap" width="60" height="60" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <section id="calon" style="margin-bottom: 200px">
+  </section>
+  <div class="container py-5">
+  </div>
+  <div class="row" id="bem">
+    <div class="col-12 py-3">
+      <h3 class="heading-2 main-color text-center">CAPRESMA & CAWAPRESMA BEM PM</h3>
+    </div>
+    <div class="col-md-6 mt-5">
+      <form action="">
+        <input type="hidden">
+        <input type="hidden">
+        <div class="card border-danger" style="min-height: 400px">
+          <div class="label">
+            <h3>#1</h3>
+          </div>
+          <img width="" src="/src/assets/logo.svg" class="img-fluid card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-left">Jovian</h5>
+            <div class="text-center">
+              <button type="button" class="btn w-100 btn-light" data-bs-toggle="modal" data-bs-target="#visi Modal">
+                Visi dan Misi
+              </button>
+              <button class="btn w-100 btn-main btn-vote ">Piluh</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="modal fade" id="visi1Modal" tabindex="-1" aria-labelledby="visi1ModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="visi1 ModalLabel">Visi dan Misi
+              Paslon No Undi 2</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <h4 class="text-center">Visi</h4>
+            wdawdjwad
+            <br>
+            <br>
+            <h4 class="text-center">Misi</h4>
+            ashdjasdhjas
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
