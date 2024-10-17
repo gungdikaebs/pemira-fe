fe5e9a2f5146ea678ffe99c73fd25706b8befebb
JovianNanda <joviansismawan13@gmail.com>
2024-10-17 20:00:12 +0800

    apifetch

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import { apiFetch } from './utils/apiFetch'
+apiFetch.get('https://dummyjson.com/auth/RESOURCE').then((res) => {
+  console.log(res)
+})
 </script>
 
 <template>
   <header>
-
-    <nav>
-
-    </nav>
-
+    <nav></nav>
   </header>
 
   <RouterView />
